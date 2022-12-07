8d7b685f42226edcb99d42ce74b0cc548222ce61
Jhony Almeida <jhony12358oficial@gmail.com>
2022-12-07 10:12:47 -0300

    UC12-EncontroRemoto2

---
+++ Classes/PessoaJurudica.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProgBackEnd.Interfaces;
+
+namespace ProgBackEnd.Classes
+{
+    public class PessoaJurudica : Pessoa, IPessoaJuridica
+    {
+        
+        public string? cnpj {get; set;}
+
+        public string? razaoSocial {get; set;}
+
+        public override float CalcularImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarCnpj(string cnpj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.Clear();
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Turma13");
+
+using ProgBackEnd.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.nome = "Jhony Almeida da Silva";
+novaPf.cpf = "1547846565";
+
+Console.WriteLine(novaPf.nome);
+Console.WriteLine(novaPf.cpf);
+Console.WriteLine($"---------------------------");
+
+
+Console.WriteLine($"Bem-vindo {novaPf.nome} seu cpf é:{novaPf.cpf}");
